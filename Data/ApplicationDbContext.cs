2339051a83dbfd8d1b79ec408fce6061c21b40ef
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-08-02 04:42:57 -0700

    finshed connection

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
