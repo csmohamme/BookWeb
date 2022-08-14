1b9882224d8b741b8eefa6c9202668fafbe47820
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-08-14 00:01:29 -0700

    finshed from create page

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace bookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
