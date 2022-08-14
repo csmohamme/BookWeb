d6a7edf14381aab9d2d36efe0e07b01377758810
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-08-14 04:50:54 -0700

    finshed Edit page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -40,5 +40,40 @@ namespace bookWeb.Controllers
             }
             return View(obj);
         }
+
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            // var CategoryFromDFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // var CategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order can not exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
